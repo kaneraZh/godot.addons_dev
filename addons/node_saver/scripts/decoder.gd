c90f8dc30f32a54ff796dffff4b35bebc5ca26b2
kaneraZh <ari1337ferrada@gmail.com>
2023-09-14 07:47:18 -0300

    decoder?
    decoder first push

---
+++ addons/node_saver/scripts/decoder.gd
@@ -0,0 +1,79 @@
+extends Resource
+#func lala():
+#	bool(false)
+#	int(0)
+#	float(0)
+#	String("")
+static func s(v):
+	if(v is Vector2):	return "v(%s, %s)"%			[v.x, v.y]	#"v(0, 0)"
+	if(v is Vector2i):	return "v(%s, %s)"%			[v.x, v.y]	#"v(0, 0)"
+	if(v is Vector3):	return "v(%s, %s, %s)"%		[v.x, v.y, v.z]	#"v(0, 0, 0)"
+	if(v is Vector3i):	return "v(%s, %s, %s)"%		[v.x, v.y, v.z]	#"v(0, 0, 0)"
+	if(v is Vector4):	return "v(%s, %s, %s, %s)"%	[v.x, v.y, v.z, v.w] #"v(0, 0, 0, 0)"
+	if(v is Vector4i):	return "v(%s, %s, %s, %s)"%	[v.x, v.y, v.z, v.w] #"v(0, 0, 0, 0)"
+	if(v is Quaternion):return "q(%s, %s, %s, %s)"%	[v.x, v.y, v.z, v.w] #"q(0, 0, 0, 1)"
+	if(v is Color):		return "c(%s, %s, %s, %s)"%	[v.r, v.g, v.b, v.a] #"c(0, 0, 0, 1)"
+	if(v is Rect2):		return "[P: %s, S: %s]"%	[s(v.position), s(v.size)]	#"[P: v(0, 0), S: v(0, 0)]"
+	if(v is Rect2i):	return "[P: %s, S: %s]"%	[s(v.position), s(v.size)]	#"[P: v(0, 0), S: v(0, 0)]"
+	if(v is Transform2D):return "[X: %s, Y: %s, O: %s]"%[s(v.x), s(v.y), s(v.origin)]	#"[X: v(1, 0), Y: v(0, 1), O: v(0, 0)]"
+	if(v is Plane):		return "[N: %s, D: %s]"%	[s(v.normal), s(v.d)] #"[N: v(0, 0, 0), D: 0]"
+	if(v is AABB):		return "[P: %s, S: %s]"%	[s(v.position), s(v.size)] #"[P: v(0, 0, 0), S: v(0, 0, 0)]"
+	if(v is Basis):		return "[X: %s, Y: %s, Z: %s]"%[s(v.x), s(v.y), s(v.z)] #"[X: v(1, 0, 0), Y: v(0, 1, 0), Z: v(0, 0, 1)]"
+#	if(v is Transform3D):return "[X: %s, Y: %s, Z: %s, O: %s]"%[s(v.origin.x), s(v.origin.y), s(v.origin.z), s(v.origin)] #"[X: (1, 0, 0), Y: (0, 1, 0), Z: (0, 0, 1), O: (0, 0, 0)]"
+#	if(v is Projection):return "\n1, 0, 0, 0\n0, 1, 0, 0\n0, 0, 1, 0\n0, 0, 0, 1" #"\n1, 0, 0, 0\n0, 1, 0, 0\n0, 0, 1, 0\n0, 0, 0, 1"
+	if(v is Transform3D):return "[B: %s, O: %s]"%[s(v.basis), s(v.origin)] #"[B: [X: v(1, 0, 0), Y: v(0, 1, 0), Z: v(0, 0, 1)], O: v(0, 0, 0)]"
+	if(v is Projection):return "[X: %s, Y: %s, Z: %s, W: %s]"%[s(v.x), s(v.y), s(v.z), s(v.w)] #"[X: v(1, 0, 0, 0), Y: v(0, 1, 0, 0), Z: v(0, 0, 1, 0), W: v(0, 0, 0, 1)]"
+	if(v is StringName):return "&%s"%v#""
+	if(v is NodePath):	return "$%s"%v#""
+	if(v is RID):		return "r%s"#"RID(0)"
+#	if(v is Object): #"<Object#null>"
+#	if(v is Callable): #"null::null"
+#	if(v is Signal): #"null::[signal]"
+	if(v is Dictionary):		return {}	#{}
+	if(v is Array):				return []	#[]
+	if(v is PackedByteArray):	return "[]"	#"[]"
+	if(v is PackedInt32Array):	return []	#[]
+	if(v is PackedInt64Array):	return []	#[]
+	if(v is PackedFloat32Array):return []	#[]
+	if(v is PackedFloat64Array):return []	#[]
+	if(v is PackedStringArray):	return []	#[]
+	if(v is PackedVector2Array):return "[]"	#"[]"
+	if(v is PackedVector3Array):return "[]"	#"[]"
+	if(v is PackedColorArray):	return "[]"	#"[]"
+
+static func d(v):
+	if(v is Vector2):	return {'x':v.x, 'y':v.y}
+	if(v is Vector2i):	return {'x':v.x, 'y':v.y}
+	if(v is Vector3):	return {'x':v.x, 'y':v.y, 'z':v.z}
+	if(v is Vector3i):	return {'x':v.x, 'y':v.y, 'z':v.z}
+	if(v is Vector4):	return {'x':v.x, 'y':v.y, 'z':v.z, 'w':v.w}
+	if(v is Vector4i):	return {'x':v.x, 'y':v.y, 'z':v.z, 'w':v.w}
+	if(v is Quaternion):return {'x':v.x, 'y':v.y, 'z':v.z, 'w':v.w}
+	if(v is Color):		return {'r':v.x, 'g':v.y, 'b':v.z, 'a':v.w}
+	if(v is Rect2):		return {'p':s(v.position), 's':s(v.size)}
+	if(v is Rect2i):	return {'p':s(v.position), 's':s(v.size)}
+	if(v is Transform2D):return{'x':s(v.x), 'y':s(v.y), 'o':s(v.origin)}
+	if(v is Plane):		return {'n':s(v.normal), 'd':s(v.d)}
+	if(v is AABB):		return {'p':s(v.position), 's':s(v.size)}
+	if(v is Basis):		return {'x':s(v.x), 'y':s(v.y), 'z':s(v.z)}
+#	if(v is Transform3D):return "[X: %s, Y: %s, Z: %s, O: %s]"%[s(v.origin.x), s(v.origin.y), s(v.origin.z), s(v.origin)] #"[X: (1, 0, 0), Y: (0, 1, 0), Z: (0, 0, 1), O: (0, 0, 0)]"
+#	if(v is Projection):return "\n1, 0, 0, 0\n0, 1, 0, 0\n0, 0, 1, 0\n0, 0, 0, 1" #"\n1, 0, 0, 0\n0, 1, 0, 0\n0, 0, 1, 0\n0, 0, 0, 1"
+	if(v is Transform3D):return{'b':s(v.basis), 'o':s(v.origin)}
+	if(v is Projection):return {'x':s(v.x), 'y':s(v.y), 'z':s(v.z), 'w':s(v.w)}
+	if(v is StringName):return "&%s"%v
+	if(v is NodePath):	return "$%s"%v
+	if(v is RID):		return "r%s"
+#	if(v is Object): #"<Object#null>"
+#	if(v is Callable): #"null::null"
+#	if(v is Signal): #"null::[signal]"
+	if(v is Dictionary):		return {}	#{}
+	if(v is Array):				return []	#[]
+	if(v is PackedByteArray):	return "[]"	#"[]"
+	if(v is PackedInt32Array):	return []	#[]
+	if(v is PackedInt64Array):	return []	#[]
+	if(v is PackedFloat32Array):return []	#[]
+	if(v is PackedFloat64Array):return []	#[]
+	if(v is PackedStringArray):	return []	#[]
+	if(v is PackedVector2Array):return "[]"	#"[]"
+	if(v is PackedVector3Array):return "[]"	#"[]"
+	if(v is PackedColorArray):	return "[]"	#"[]"
---
+++ run.gd
@@ -0,0 +1,156 @@
+@tool
+extends EditorScript
+
+const TYPES_STRING:PackedStringArray = [
+	"bool",
+	"int",
+	"float",
+	"String",
+	"Vector2",
+	"Vector2i",
+	"Rect2",
+	"Rect2i",
+	"Vector3",
+	"Vector3i",
+	"Transform2D",
+	"Vector4",
+	"Vector4i",
+	"Plane",
+	"Quaternion",
+	"AABB",
+	"Basis",
+	"Transform3D",
+	"Projection",
+	"Color",
+	"StringName",
+	"NodePath",
+	"RID",
+	"Object",
+	"Callable",
+	"Signal",
+	"Dictionary",
+	"Array",
+	"PackedByteArray",
+	"PackedInt32Array",
+	"PackedInt64Array",
+	"PackedFloat32Array",
+	"PackedFloat64Array",
+	"PackedStringArray",
+	"PackedVector2Array",
+	"PackedVector3Array",
+	"PackedColorArray"
+]
+static var TYPES_EMPTY:Array = [
+	bool(),
+	int(),
+	float(),
+	String(),
+	Vector2(),
+	Vector2i(),
+	Rect2(),
+	Rect2i(),
+	Vector3(),
+	Vector3i(),
+	Transform2D(),
+	Vector4(),
+	Vector4i(),
+	Plane(),
+	Quaternion(),
+	AABB(),
+	Basis(),
+	Transform3D(),
+	Projection(),
+	Color(),
+	StringName(),
+	NodePath(),
+	RID(),
+	Object(),
+	Callable(),
+	Signal(),
+	Dictionary(),
+	Array(),
+	PackedByteArray(),
+	PackedInt32Array(),
+	PackedInt64Array(),
+	PackedFloat32Array(),
+	PackedFloat64Array(),
+	PackedStringArray(),
+	PackedVector2Array(),
+	PackedVector3Array(),
+	PackedColorArray()
+]
+
+func stringed():
+	print("-------------%s\n%s"%["bool", JSON.stringify(bool())])
+	print("-------------%s\n%s"%["int", JSON.stringify(int())])
+	print("-------------%s\n%s"%["float", JSON.stringify(float())])
+	print("-------------%s\n%s"%["String", JSON.stringify(String())])
+	print("-------------%s\n%s"%["Vector2", JSON.stringify(Vector2())])
+	print("-------------%s\n%s"%["Vector2i", JSON.stringify(Vector2i())])
+	print("-------------%s\n%s"%["Rect2", JSON.stringify(Rect2())])
+	print("-------------%s\n%s"%["Rect2i", JSON.stringify(Rect2i())])
+	print("-------------%s\n%s"%["Vector3", JSON.stringify(Vector3())])
+	print("-------------%s\n%s"%["Vector3i", JSON.stringify(Vector3i())])
+	print("-------------%s\n%s"%["Transform2D", JSON.stringify(Transform2D())])
+	print("-------------%s\n%s"%["Vector4", JSON.stringify(Vector4())])
+	print("-------------%s\n%s"%["Vector4i", JSON.stringify(Vector4i())])
+	print("-------------%s\n%s"%["Plane", JSON.stringify(Plane())])
+	print("-------------%s\n%s"%["Quaternion", JSON.stringify(Quaternion())])
+	print("-------------%s\n%s"%["AABB", JSON.stringify(AABB())])
+	print("-------------%s\n%s"%["Basis", JSON.stringify(Basis())])
+	print("-------------%s\n%s"%["Transform3D", JSON.stringify(Transform3D())])
+	print("-------------%s\n%s"%["Projection", JSON.stringify(Projection())])
+	print("-------------%s\n%s"%["Color", JSON.stringify(Color())])
+	print("-------------%s\n%s"%["StringName", JSON.stringify(StringName())])
+	print("-------------%s\n%s"%["NodePath", JSON.stringify(NodePath())])
+	print("-------------%s\n%s"%["RID", JSON.stringify(RID())])
+	print("-------------%s\n%s"%["Object", JSON.stringify(Object())])
+	print("-------------%s\n%s"%["Callable", JSON.stringify(Callable())])
+	print("-------------%s\n%s"%["Signal", JSON.stringify(Signal())])
+	print("-------------%s\n%s"%["Dictionary", JSON.stringify(Dictionary())])
+	print("-------------%s\n%s"%["Array", JSON.stringify(Array())])
+	print("-------------%s\n%s"%["PackedByteArray", JSON.stringify(PackedByteArray())])
+	print("-------------%s\n%s"%["PackedInt32Array", JSON.stringify(PackedInt32Array())])
+	print("-------------%s\n%s"%["PackedInt64Array", JSON.stringify(PackedInt64Array())])
+	print("-------------%s\n%s"%["PackedFloat32Array", JSON.stringify(PackedFloat32Array())])
+	print("-------------%s\n%s"%["PackedFloat64Array", JSON.stringify(PackedFloat64Array())])
+	print("-------------%s\n%s"%["PackedStringArray", JSON.stringify(PackedStringArray())])
+	print("-------------%s\n%s"%["PackedVector2Array", JSON.stringify(PackedVector2Array())])
+	print("-------------%s\n%s"%["PackedVector3Array", JSON.stringify(PackedVector3Array())])
+	print("-------------%s\n%s"%["PackedColorArray", JSON.stringify(PackedColorArray())])
+func string_parse():
+	print("---------bool\n%s"%JSON.parse_string( JSON.stringify(bool())))
+	print("---------float\n%s"%JSON.parse_string( JSON.stringify(float())))
+	print("---------String\n%s"%JSON.parse_string( JSON.stringify(String())))
+	print("---------Dictionary\n%s"%JSON.parse_string( JSON.stringify(Dictionary())))
+	print("---------Array\n%s"%JSON.parse_string( JSON.stringify(Array())))
+func string_parse_typecast():
+	print("---------bool\t:\t%s\n%s"%[bool(JSON.parse_string( JSON.stringify(bool()))) is bool, JSON.parse_string( JSON.stringify(bool()))] )
+	print("---------int\t:\t%s\n%s"%[int(JSON.parse_string( JSON.stringify(int()))) is int, JSON.parse_string( JSON.stringify(int()))] )
+	print("---------float\t:\t%s\n%s"%[float(JSON.parse_string( JSON.stringify(float()))) is float, JSON.parse_string( JSON.stringify(float()))] )
+	print("---------String\t:\t%s\n%s"%[String(JSON.parse_string( JSON.stringify(String()))) is String, JSON.parse_string( JSON.stringify(String()))] )
+	print("---------StringName\t:\t%s\n%s"%[StringName(JSON.parse_string( JSON.stringify(StringName()))) is StringName, JSON.parse_string( JSON.stringify(StringName()))] )
+	print("---------NodePath\t:\t%s\n%s"%[NodePath(JSON.parse_string( JSON.stringify(NodePath()))) is NodePath, JSON.parse_string( JSON.stringify(NodePath()))] )
+	print("---------Dictionary\t:\t%s\n%s"%[Dictionary(JSON.parse_string( JSON.stringify(Dictionary()))) is Dictionary, JSON.parse_string( JSON.stringify(Dictionary()))] )
+	print("---------Array\t:\t%s\n%s"%[Array(JSON.parse_string( JSON.stringify(Array()))) is Array, JSON.parse_string( JSON.stringify(Array()))] )
+	print("---------PackedInt32Array\t:\t%s\n%s"%[PackedInt32Array(JSON.parse_string( JSON.stringify(PackedInt32Array()))) is PackedInt32Array, JSON.parse_string( JSON.stringify(PackedInt32Array()))] )
+	print("---------PackedInt64Array\t:\t%s\n%s"%[PackedInt64Array(JSON.parse_string( JSON.stringify(PackedInt64Array()))) is PackedInt64Array, JSON.parse_string( JSON.stringify(PackedInt64Array()))] )
+	print("---------PackedFloat32Array\t:\t%s\n%s"%[PackedFloat32Array(JSON.parse_string( JSON.stringify(PackedFloat32Array()))) is PackedFloat32Array, JSON.parse_string( JSON.stringify(PackedFloat32Array()))] )
+	print("---------PackedFloat64Array\t:\t%s\n%s"%[PackedFloat64Array(JSON.parse_string( JSON.stringify(PackedFloat64Array()))) is PackedFloat64Array, JSON.parse_string( JSON.stringify(PackedFloat64Array()))] )
+	print("---------PackedStringArray\t:\t%s\n%s"%[PackedStringArray(JSON.parse_string( JSON.stringify(PackedStringArray()))) is PackedStringArray, JSON.parse_string( JSON.stringify(PackedStringArray()))] )
+
+func _run():
+	var regex:RegEx = RegEx.create_from_string("\"(\\d)(\\d)(\\d)\"")
+#	create_from_string("\"(?:\\\\.|[^\"])*\"")
+#	compile("/(\d)(\d)(\d)")
+#	RegEx.create_from_string("(\d)(\d)(\d)")
+	
+#	var res:Array[RegExMatch] = regex.search_all("123abc")
+#	for m in res:
+#		print(m)#.strings)
+	
+#	var res:RegExMatch = regex.search("123abc")
+#	print(res)
+	
+	print(regex.compile("123abc"))
+	print(regex)
